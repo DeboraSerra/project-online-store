0a3f928838ff4c48197aa8504ce34e5ca50a3475
Débora Serra <debora.r.serra@gmail.com>
2022-03-15 18:57:34 -0300

    Updates

---
+++ precache-manifest.5f6ec5db9826f57274d4e4ac2968f12f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4b45ec04d2b3ea6e6fba50485ce87e3",
+    "url": "/project-online-store/index.html"
+  },
+  {
+    "revision": "60ab9fc8aa6dc7499384",
+    "url": "/project-online-store/static/css/main.d58915b4.chunk.css"
+  },
+  {
+    "revision": "65980455eeab3eb81ad8",
+    "url": "/project-online-store/static/js/2.7e1c9b54.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/project-online-store/static/js/2.7e1c9b54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60ab9fc8aa6dc7499384",
+    "url": "/project-online-store/static/js/main.178f3361.chunk.js"
+  },
+  {
+    "revision": "3b38855b13c2b2508642",
+    "url": "/project-online-store/static/js/runtime-main.1dc74610.js"
+  }
+]);
