16fc839095f4f39d22b1cfb407706412c5f46ac1
Débora Serra <debora.r.serra@gmail.com>
2022-03-17 12:45:58 -0300

    Adiciona readme e link na pagina para o codiigo base

--- src/App.js
+++ src/App.js
@@ -11,16 +11,21 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Switch>
-          <Route exact path="/project-online-store/" component={ Search } />
-          <Route path="/project-online-store/shopping-cart" component={ ShoppingCart } />
-          <Route
-            path="/project-online-store/product/:id/evaluation-forms"
-            component={ AvaliatorProd }
-          />
-          <Route path="/project-online-store/product/:id" component={ ProductDetail } />
-          <Route path="/project-online-store/checkout" component={ Checkout } />
-        </Switch>
+        <section className="main-page">
+          <Switch>
+            <Route exact path="/project-online-store/" component={ Search } />
+            <Route path="/project-online-store/shopping-cart" component={ ShoppingCart } />
+            <Route
+              path="/project-online-store/product/:id/evaluation-forms"
+              component={ AvaliatorProd }
+            />
+            <Route path="/project-online-store/product/:id" component={ ProductDetail } />
+            <Route path="/project-online-store/checkout" component={ Checkout } />
+          </Switch>
+        </section>
+        <footer className="footer">
+          See our codebase in <a href="https://github.com/DeboraSerra/project-online-store/tree/master">GitHub</a>
+        </footer>
       </BrowserRouter>
     );
   }
